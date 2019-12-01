38e37067e8338ada5546239c2b1dad3b6eafa2d5
Sela Marjan Almog <selama@wix.com>
2019-12-01 19:43:08 +0200

    day 1 complete

--- src/day-1.ts
+++ src/day-1.ts
@@ -1,7 +1,15 @@
 const getFuelReq = (num: number) => Math.floor(num / 3) - 2;
 
+const calcFuelReq = (num: number) => {
+  const fr: number = getFuelReq(num);
+  if (fr <= 0) {
+    return 0;
+  }
+  return fr + calcFuelReq(fr);
+}
+
 export const sumFuelRequeirements = (arr: number[]) => {
   return arr.reduce((sum, current) => {
-    return sum + getFuelReq(current);
+    return sum + calcFuelReq(current);
   }, 0);
 };
